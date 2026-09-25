3e9b3331a06f57d7e26a434a31d1d278c46f82cf
cristian <tucrodhid1612b@g.educaand.es>
2026-09-25 14:55:31 +0200

    primera prueba de servidor en la web de proyectos de clase

--- web/aplicacion/pruebas/basicas.php
+++ web/aplicacion/pruebas/basicas.php
@@ -1,27 +1,26 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-$usuario=getenv("MYSQL_USER");
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
-inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+inicioCuerpo("Pruebas básicas");
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
-
 //vista
-function cabecera() 
+function cabecera()
 {}
-
 //vista
 function cuerpo()
 {
 ?>
-    <br><br>
-    <a href="./aplicacion/pruebas/index.php">Acceso a pruebas</a>
+<br><br>
+Esto es html
+<?php 
+    echo "esto esta hecho";
+ ?>
 <?php
+
 }
--- web/aplicacion/pruebas/index.php
+++ web/aplicacion/pruebas/index.php
@@ -1,27 +1,30 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
 
-$usuario=getenv("MYSQL_USER");
+
 
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
-inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+inicioCuerpo("Pruebas básicas");
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
 
+
 //vista
-function cabecera() 
+function cabecera()
 {}
 
 //vista
 function cuerpo()
 {
 ?>
+<br><br>
+    Elemento de prueba
     <br><br>
-    <a href="./aplicacion/pruebas/index.php">Acceso a pruebas</a>
+    <a href="basicas.php">Funcionamiento básico</a>
 <?php
 }
